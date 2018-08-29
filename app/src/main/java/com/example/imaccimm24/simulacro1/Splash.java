97d9117092d7b03b2a8053afc86a17f6d5a4ea61
imaccimm24 <jabautista065>
2018-08-29 09:41:07 -0500

    menu overflow

--- app/src/main/java/com/example/imaccimm24/simulacro1/Splash.java
+++ app/src/main/java/com/example/imaccimm24/simulacro1/Splash.java
@@ -1,5 +1,7 @@
 package com.example.imaccimm24.simulacro1;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +11,16 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent menu = new Intent(Splash.this, menuP.class);
+                startActivity(menu);
+                finish();
+            }
+        },3000);
     }
-    //1
+
 }
--- app/src/main/java/com/example/imaccimm24/simulacro1/menuP.java
+++ app/src/main/java/com/example/imaccimm24/simulacro1/menuP.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import java.io.Serializable;
 
@@ -12,6 +16,7 @@ public class menuP extends AppCompatActivity {
 
     CardView queVisitar,dondeComer,queHacer,dondeDormir,Servicios,Llamadas;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_p);
@@ -63,6 +68,39 @@ public class menuP extends AppCompatActivity {
                 startActivity(uno);
             }
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Intent registrar = new Intent(this, Registrar.class);
+                startActivity(registrar);
+                return true;
+
+            case R.id.item2:
+                Intent ayudar = new Intent(this, Ayudar.class);
+                startActivity(ayudar);
+                return true;
+
+            case R.id.item3:
+                Toast.makeText(this, "bye", Toast.LENGTH_SHORT).show();
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
     }
 
 }

0d196c07cead3252424633e9a243bc74ff7492e2
imaccimm24 <jabautista065>
2018-08-29 09:57:08 -0500

    union de junior

--- app/src/main/java/com/example/imaccimm24/simulacro1/Informacion.java
+++ app/src/main/java/com/example/imaccimm24/simulacro1/Informacion.java
@@ -1,7 +1,12 @@
 package com.example.imaccimm24.simulacro1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Informacion extends AppCompatActivity {
 
@@ -9,5 +14,38 @@ public class Informacion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacion);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.item1:
+                Intent registar = new Intent(this, Registrar.class);
+                startActivity(registar);
+                return true;
+
+            case R.id.item2:
+                Intent ayudar = new Intent(this, Ayudar.class);
+                startActivity(ayudar);
+                return true;
+
+            case R.id.item3:
+                Toast.makeText(this, "bye", Toast.LENGTH_SHORT).show();
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }

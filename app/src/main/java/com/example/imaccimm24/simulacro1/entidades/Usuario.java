b96f40c54ed4dce0dae09ea9e1d695586b6851c2
JuniorBonilla <juniorbonilla100@gmail.com>
2018-08-30 07:26:46 -0500

    CRU

--- app/src/main/java/com/example/imaccimm24/simulacro1/entidades/Usuario.java
+++ app/src/main/java/com/example/imaccimm24/simulacro1/entidades/Usuario.java
@@ -1,5 +1,12 @@
 package com.example.imaccimm24.simulacro1.entidades;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.constraint.solver.widgets.Helper;
+
+import com.example.imaccimm24.simulacro1.Helper.HelperSqlite;
+
 import java.io.Serializable;
 
 public class Usuario implements Serializable {
@@ -42,4 +49,34 @@ public class Usuario implements Serializable {
         Email = email;
     }
 
-}
+    public float Registrar(Context context){
+        HelperSqlite base =new HelperSqlite(context,"bd_simulacro.db",null,1);
+        SQLiteDatabase datos = base.getWritableDatabase();
+        ContentValues values= new ContentValues();
+        values.put("Nombre",Nombre);
+        values.put("Telefono",Telefono);
+        values.put("Email",Email);
+        long uno = datos.insert("Usuario",null,values);
+        return uno;
+    }
+    public float Modificar(Context context) {
+        HelperSqlite base = new HelperSqlite(context, "bd_simulacro.db", null, 1);
+        SQLiteDatabase datos = base.getWritableDatabase();
+        ContentValues values= new ContentValues();
+        values.put("Nombre",Nombre);
+        values.put("Telefono",Telefono);
+        values.put("Email",Email);
+        long uno = datos.update("Uusario",values,"Nombre="+Nombre,null);
+        return uno;
+    }
+
+    public float Eliminar(Context context){
+        HelperSqlite base = new HelperSqlite(context, "bd_simulacro.db", null, 1);
+        SQLiteDatabase datos = base.getWritableDatabase();
+        long uno =  datos.delete("Usuario","Nombre="+Nombre,null);
+
+        return uno;
+
+    }
+    }
+

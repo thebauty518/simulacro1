b95da97b3a19862650002f54377e83f6a669899b
JuniorBonilla <juniorbonilla100@gmail.com>
2018-08-30 07:05:42 -0500

    sqlitehelper

---
+++ app/src/main/java/com/example/imaccimm24/simulacro1/Helper/HelperSqlite.java
@@ -0,0 +1,36 @@
+package com.example.imaccimm24.simulacro1.Helper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class HelperSqlite extends SQLiteOpenHelper{
+    public HelperSqlite(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE Usuario (\n" +
+                "    Nonmbre  TEXT,\n" +
+                "    Telefono TEXT,\n" +
+                "    Email    TEXT\n" +
+                ")");
+        sqLiteDatabase.execSQL("CREATE TABLE Items (\n" +
+                "    Nombre      TEXT,\n" +
+                "    Descripcion TEXT,\n" +
+                "    Telefono    TEXT,\n" +
+                "    Direccion   TEXT,\n" +
+                "    Web         TEXT,\n" +
+                "    Email       TEXT,\n" +
+                "    Categoria   TEXT REFERENCES Categoria_item (Categoria),\n" +
+                "    Tipo        TEXT REFERENCES Tipo_Item (Tipo) \n" +
+                ")");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
